0fc10bee9daf093b6b58f0a6bcf0c828066c9684
Alex <alexduval71@gmail.com>
2016-08-08 16:39:50 +0200

    translate in english add "play agin" btn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/game' => 'biggle#game'
   get '/score' => 'biggle#score'
-  root 'biggle#game'
+  root 'biggle#game', as: 'home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
