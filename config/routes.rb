868357399c135a0fe150e32105fb22400a0b144c
Alex <alexduval71@gmail.com>
2016-08-08 16:35:19 +0200

    set home directly on /game

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/game' => 'biggle#game'
   get '/score' => 'biggle#score'
+  root 'biggle#game'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
